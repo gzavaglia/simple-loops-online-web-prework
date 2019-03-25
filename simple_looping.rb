6d0c0130b84e12070e98f931e0ef694fb1eff130
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-25 20:12:21 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,11 +12,11 @@ def loop_iterator(number_of_times)
   counter = 0 
   loop do
     puts phrase
+    counter += 1 
     if counter == number_of_times
       break
     end
   end
-  
 end
 
 def times_iterator(number_of_times)
